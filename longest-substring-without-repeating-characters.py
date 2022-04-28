43e7f3d47d6d9764e3b044f1450faa3e25d7ca19
Tanay Roman <tanayroman123@gmail.com>
2022-04-27 20:19:05 -0500

    debug

--- longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters.py
@@ -1,17 +1,22 @@
-# runtime:
+# runtime: 37 ms
 
-class Solution:
-    def lengthOfLongestSubstring(self,s):
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         head = max_length = 0
         seen = {}
 
         for index,c in enumerate(s):
             if c in seen and head <= seen[c]:
-                start = used[c] + 1
+                head = seen[c] + 1
 
             else:
-                max_length = max(max_length, i - start + 1)
-                seen[c] = i
+                max_length = max(max_length, index - head + 1)
+            
+            seen[c] = index
 
         return max_length
 
