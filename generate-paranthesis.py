07a40f0c7d062f217599c5a604ffdfd77f58f622
Tanay Roman <tanayroman123@gmail.com>
2022-05-02 01:38:40 -0500

    generate paranthesis

---
+++ generate-paranthesis.py
@@ -0,0 +1,16 @@
+# Not my solution. All credit to "https://leetcode.com/problems/generate-parentheses/discuss/10369/Clean-Python-DP-Solution".
+# Just a really elegant solution I wanted to remember.
+
+
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        dp = [[] for i in range(n + 1)]
+        dp[0].append('')
+        for i in range(n + 1):
+            for j in range(i):
+                dp[i] += ['(' + x + ')' + y for x in dp[j] for y in dp[i - j - 1]]
+        return dp[n]
